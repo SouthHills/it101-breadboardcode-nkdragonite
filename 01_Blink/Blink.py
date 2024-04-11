a7a7454b7afce201e82097622202e6160b5cab5e
nkdragonite <cta76@southhills.edu>
2024-04-11 15:23:39 -0400

    lab 7 submit

--- 01_Blink/Blink.py
+++ 01_Blink/Blink.py
@@ -2,16 +2,24 @@ from gpiozero import LED as LEDClass # Alias
 import time
 
 LED = LEDClass(17)  # define led
+LED2 = LEDClass(27)
 
 def loop():
     global LED
+    global LED2
     while True:
         LED.on() 
         print ("led turned on >>>") # print information on terminal
         time.sleep(1)
         LED.off()
         print ("led turned off <<<")
+        LED2.on()
+        print ("led 2 turned on >>>") # print information on terminal
         time.sleep(1)
+        LED2.off()
+        print ("led 2 turned off <<<")
+        
+        
         
 def destroy():
     global LED
